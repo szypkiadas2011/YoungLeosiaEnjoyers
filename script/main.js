c940c76a8c961dbe5d3368f65214932997644dc5
nonek <adambimek@gmail.com>
2022-11-26 23:53:34 +0100

    change onclick method fetch r/all by default rename file

---
+++ script/main.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+	document.getElementById("searchbtn").onclick = () => {
+		fetchSubreddit(document.getElementById("searchbar").value);
+	}
+
+	fetchSubreddit("all");
+}
+
+function fetchSubreddit(sub)
+{
+	let parentDiv = document.getElementById('posts')
+	parentDiv.innerHTML = ""
+	fetch(`https://www.reddit.com/r/${sub}.json`)
+		.then(response => response.json())
+		.then(body => {
+			for (let index = 0; index < body.data.children.length; index++)
+			{
+				let div = document.createElement('div')
+				let h4 = document.createElement('h4')
+				let p = document.createElement('p')
+				h4.textContent = body.data.children[index].data.title
+				if (body.data.children[index].data.post_hint === 'image')
+				{
+					let image = document.createElement('img')
+					image.src = body.data.children[index].data.url_overridden_by_dest
+					div.appendChild(image)
+				}
+				div.appendChild(h4)
+				parentDiv.appendChild(div)
+
+			}
+			document.body.appendChild(parentDiv)
+		})
+}
