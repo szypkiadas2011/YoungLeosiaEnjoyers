28ecf43f49526974b0891c47e380381b4307bdf0
nonek <adambimek@gmail.com>
2022-11-27 00:19:18 +0100

    kurka fix

--- script/utils.js
+++ script/utils.js
@@ -1,13 +1,13 @@
 export const compose = (...fns) => x => fns.reduce((acc, fn) => fn(acc), x);
 
-
-
 export const appendElement = (parent, type, content) => {
 	let element = document.createElement(type);
-	parent.appendChild(element.innerHTML = content);
+	element.innerHTML = content;
+	document.getElementById(parent).appendChild(element);
 };
 
 export const appendImage = (parent, url) => {
-	let element = parent.createElement("img");
-	document.appendChild(element.src = url);
+	let element = document.createElement("img");
+	element.src = url;
+	document.getElementById(parent).appendChild(element);
 }
