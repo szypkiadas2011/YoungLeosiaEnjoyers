ae1cbcc8ed60e7ba0f9cf9c7a84e1abb8ac3db13
nonek <adambimek@gmail.com>
2022-11-27 21:46:07 +0100

    formatting

--- script/rules.js
+++ script/rules.js
@@ -1,15 +1,18 @@
-import{appendElement, formatHtml} from './utils.js';
-export function fetchRules(sub) {
-    return fetch(`https://www.reddit.com/r/${sub}/about/rules.json`)
-        .then(res => res.json())
-        .then(json => json.rules)
+import { appendElement, formatHtml } from './utils.js';
+
+export function fetchRules(sub)
+{
+	return fetch(`https://www.reddit.com/r/${sub}/about/rules.json`)
+		.then(res => res.json())
+		.then(json => json.rules)
 }
 
-export function renderRules(rules) {
-    const rulesDiv = document.getElementById("rules");
-    rulesDiv.innerHTML = "";
-    rules.forEach(r => {
-        appendElement(rulesDiv, "h3","",r.short_name);
-        appendElement(rulesDiv, "p","",formatHtml(r.description_html));
-    });
+export function renderRules(rules)
+{
+	const rulesDiv = document.getElementById("rules");
+	rulesDiv.innerHTML = "";
+	rules.forEach(r => {
+		appendElement(rulesDiv, "h3", "", r.short_name);
+		appendElement(rulesDiv, "p", "", formatHtml(r.description_html));
+	});
 }
