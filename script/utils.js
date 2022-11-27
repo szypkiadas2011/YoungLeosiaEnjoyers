304d3080de7a6b79308b4e260c90930a4d03f894
nonek <adambimek@gmail.com>
2022-11-27 02:34:14 +0100

    f go back

--- script/utils.js
+++ script/utils.js
@@ -3,11 +3,11 @@ export const compose = (...fns) => x => fns.reduce((acc, fn) => fn(acc), x);
 export const appendElement = (parent, type, content) => {
 	let element = document.createElement(type);
 	element.innerHTML = content;
-	document.getElementById(parent).appendChild(element);
+	parent.appendChild(element);
 };
 
 export const appendImage = (parent, url) => {
 	let element = document.createElement("img");
 	element.src = url;
-	document.getElementById(parent).appendChild(element);
+	parent.appendChild(element);
 }
